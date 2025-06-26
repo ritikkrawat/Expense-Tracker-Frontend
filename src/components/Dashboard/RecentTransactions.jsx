b7ba4e85fa7e3c4e9672c17d4a20a817b64bf419
Ritik Rawat <rithikrawat2004@gmail.com>
2025-06-26 12:44:54 +0530

    Source Code

--- src/components/Dashboard/RecentTransactions.jsx
+++ src/components/Dashboard/RecentTransactions.jsx
@@ -5,7 +5,7 @@ import TransactionInfoCard from '../Cards/TransactionInfoCard';
 
 const RecentTransactions = ({ transactions, onSeeMore }) => {
   return (
-    <div className="card">
+    <div className="card transition-all duration-200 hover:shadow-xl hover:scale-[1.01] hover:border hover:border-blue-100 hover:bg-blue-50/20">
       <div className="flex items-center justify-between">
         <h5 className="text-lg">Recent Transactions</h5>
 
