980c84622b5f6faf18c71cd28a445f98ad46d0f9
Ritik Rawat <rithikrawat2004@gmail.com>
2025-06-26 12:52:33 +0530

    Source Code

--- src/components/Income/IncomeList.jsx
+++ src/components/Income/IncomeList.jsx
@@ -5,7 +5,7 @@ import moment from 'moment'
 
 const IncomeList = ({transactions, onDelete, onDownload}) => {
   return (
-    <div className='card'>
+    <div className='card transition-all duration-200 hover:shadow-xl hover:scale-[1.01] hover:border hover:border-blue-100 hover:bg-blue-50/20'>
         <div className='flex items-center justify-between'>
             <h5 className='text-lg'>Income Sources</h5>
 
