6cf83a6c86aac786673867dd81baf06091dd66b2
Ritik Rawat <rithikrawat2004@gmail.com>
2025-06-28 20:03:57 +0530

    Source Code

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,8 +1,8 @@
-import React , {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import AuthLayout from '../../components/layouts/AuthLayout';
-import {Link, useNavigate} from 'react-router-dom';
-import Input from '../../components/Inputs/Input'
-import {validateEmail} from '../../utils/helper';
+import { Link, useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import { validateEmail } from '../../utils/helper';
 import axiosInstance from '../../utils/axiosinstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import { UserContext } from '../../context/userContext';
@@ -11,90 +11,85 @@ import toast from 'react-hot-toast';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
 
   const { updateUser } = useContext(UserContext);
-
   const navigate = useNavigate();
 
-  // Handle Login Form Submit
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    if(!validateEmail(email)) {
+    if (!validateEmail(email)) {
       toast.error('Please enter a valid email address.');
       return;
     }
 
-    if(!password) {
+    if (!password) {
       toast.error('Please enter the password');
       return;
     }
 
-    // Login API Call
     try {
       const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
         email,
         password,
       });
+
       const { token, user } = response.data;
-      
+
       if (token) {
-        localStorage.setItem("token", token);
+        localStorage.setItem('token', token);
         updateUser(user);
         toast.success('Login successful!');
-        navigate("/dashboard");
-      } 
+        navigate('/dashboard');
+      }
     } catch (error) {
       console.log(error);
-      if (error.response && error.response.data.message) {
+      if (error.response?.data?.message) {
         toast.error(error.response.data.message);
       } else {
-        toast.error("Something went wrong. Please try again.");
+        toast.error('Something went wrong. Please try again.');
       }
     }
   };
-  
+
   return (
     <AuthLayout>
-      <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
-        <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
-        <p className='text-xs text-slate-700 mt-[5px] mb-6'> 
-          Please enter your credentials to log in  
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your credentials to log in
         </p>
-      
+
         <form onSubmit={handleLogin}>
           <Input
             value={email}
-            onChange={({target}) => setEmail(target.value)}
-            label='Email Address'
-            placeholder='example@gmail.com'
-            type='text'
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="example@gmail.com"
+            type="text"
           />
 
           <Input
             value={password}
-            onChange={({target}) => setPassword(target.value)}
-            label='Password '
-            placeholder='Min 8 characters'
-            type='password'
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password "
+            placeholder="Min 8 characters"
+            type="password"
           />
 
-          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
-
-          <button type='submit' className='btn-primary'>
+          <button type="submit" className="btn-primary">
             LOGIN
           </button>
 
-          <p className='text=[13px] text-slate-800 mt=3'>
-            Don't have an account?{' '}
-            <Link className='font-medium text-primary underline' to='/signup'>
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don&apos;t have an account?{' '}
+            <Link className="font-medium text-primary underline" to="/signup">
               Signup
             </Link>
           </p>
         </form>
-      </div> 
-    </AuthLayout>    
+      </div>
+    </AuthLayout>
   );
 };
 
