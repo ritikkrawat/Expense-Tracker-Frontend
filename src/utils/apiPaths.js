b9475049a1b6b97fb7f93da3f4e705c3d0e84628
Ritik Rawat <rithikrawat2004@gmail.com>
2025-12-10 17:11:52 +0530

    Update apiPaths.js

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "process.env.REACT_APP_API_URL";
+export const BASE_URL = "https://expense-tracker-backend-livid.vercel.app";
 
 // utils/apiPaths.js
 export const API_PATHS = {
